c56d014c6fec943c674381dec028d0adfffcfac5
tsodre-p <tsodre-p@student.42porto.com>
2023-10-03 10:23:37 +0100

    cpp_module_00 files

---
+++ cpp_module_00/ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsodre-p <tsodre-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 10:01:47 by tsodre-p          #+#    #+#             */
+/*   Updated: 2023/10/03 10:21:41 by tsodre-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <locale>
+#include <stdio.h>
+
+int	main(int argc, char **argv)
+{
+	int	j;
+
+	if (argc == 1)
+		std::cout << " * LOUD AND UNBEARABLE FEEDBACK NOISE * ";
+	else
+	{
+		for(int i; i < argc; i++)
+		{
+			j = 0;
+			if (argv[i][j])
+		}
+	}
+}
