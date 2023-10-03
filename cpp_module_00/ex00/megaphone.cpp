c37179b73babe5fc679c2e4e20ecd59e03bdd534
Tiago Pereira <tiagosodrepereira@gmail.com>
2023-10-03 12:25:46 +0100

    exercice done

--- cpp_module_00/ex00/megaphone.cpp
+++ cpp_module_00/ex00/megaphone.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tsodre-p <tsodre-p@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tsodre-p <tsodre-p@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/03 10:01:47 by tsodre-p          #+#    #+#             */
-/*   Updated: 2023/10/03 10:21:41 by tsodre-p         ###   ########.fr       */
+/*   Created: 2023/10/03 11:11:36 by tsodre-p          #+#    #+#             */
+/*   Updated: 2023/10/03 12:25:16 by tsodre-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,17 @@
 
 int	main(int argc, char **argv)
 {
-	int	j;
-
 	if (argc == 1)
 		std::cout << " * LOUD AND UNBEARABLE FEEDBACK NOISE * ";
 	else
 	{
-		for(int i; i < argc; i++)
+		for(int i = 1; i < argc; i++)
 		{
-			j = 0;
-			if (argv[i][j])
+			for(int j = 0; j < argv[i][j]; j++)
+				std::cout << (char)toupper(argv[i][j]);
+			std::cout << " ";
 		}
 	}
+	std::cout << std::endl;
+	return (0);
 }
