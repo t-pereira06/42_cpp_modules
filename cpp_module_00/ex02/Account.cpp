14d65a4da6b051dc1ac2fe441d68b61fe8b7ea4e
tsodre-p <tsodre-p@student.42porto.com>
2023-10-12 16:14:04 +0100

    changes to ex02 files

--- cpp_module_00/ex02/Account.cpp
+++ cpp_module_00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: tsodre-p <tsodre-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 14:25:51 by tsodre-p          #+#    #+#             */
-/*   Updated: 2023/10/12 15:20:27 by tsodre-p         ###   ########.fr       */
+/*   Updated: 2023/10/12 15:49:51 by tsodre-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,20 @@ Account::Account(int initial_deposit)
 	this->_nbWithdrawals = 0;
 	Account::_totalAmount += initial_deposit;
 	Account::_displayTimestamp();
-	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";created" << std::endl;
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "created" << std::endl;
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	this->_amount += deposit;
+	Account::_totalAmount += deposit;
+	Account::_totalNbDeposits++;
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "created" << std::endl;
 }
 
 int	Account::getNbAccounts()
