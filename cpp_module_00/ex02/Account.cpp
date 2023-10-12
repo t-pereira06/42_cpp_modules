f85df167a05a7bdb803c832c9d087558c99be86b
tsodre-p <tsodre-p@student.42porto.com>
2023-10-12 15:20:44 +0100

    changes to ex02 files

--- cpp_module_00/ex02/Account.cpp
+++ cpp_module_00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: tsodre-p <tsodre-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 14:25:51 by tsodre-p          #+#    #+#             */
-/*   Updated: 2023/10/12 14:57:51 by tsodre-p         ###   ########.fr       */
+/*   Updated: 2023/10/12 15:20:27 by tsodre-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,4 +40,24 @@ Account::Account(int initial_deposit)
 	Account::_totalAmount += initial_deposit;
 	Account::_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";created" << std::endl;
+}
+
+int	Account::getNbAccounts()
+{
+	return (Account::_nbAccounts);
+}
+
+int	Account::getTotalAmount()
+{
+	return (Account::_totalAmount);
+}
+
+int	Account::getNbDeposits()
+{
+	return (Account::_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals()
+{
+	return (Account::_nbAccounts);
 }
