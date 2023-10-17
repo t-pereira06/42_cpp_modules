44aa1314453cdce12631b8e35521ba786aadd4e4
tsodre-p <tsodre-p@student.42porto.com>
2023-10-17 09:33:58 +0100

    final changes

--- cpp_module_00/ex00/megaphone.cpp
+++ cpp_module_00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: tsodre-p <tsodre-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 11:11:36 by tsodre-p          #+#    #+#             */
-/*   Updated: 2023/10/16 15:03:05 by tsodre-p         ###   ########.fr       */
+/*   Updated: 2023/10/17 09:24:49 by tsodre-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 int	main(int argc, char **argv)
 {
 	if (argc == 1)
-		std::cout << " * LOUD AND UNBEARABLE FEEDBACK NOISE * ";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 	{
 		for(int i = 1; i < argc; i++)
