786c6d5b7928286b7ed992cc742ea8e333ddef5e
Tiago Pereira <tiagosodrepereira@gmail.com>
2023-11-02 16:46:49 +0000

    changes to ex03 files

--- cpp_module_02/ex03/src/bsp.cpp
+++ cpp_module_02/ex03/src/bsp.cpp
@@ -6,7 +6,13 @@
 /*   By: tsodre-p <tsodre-p@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 16:05:09 by tsodre-p          #+#    #+#             */
-/*   Updated: 2023/11/02 16:05:12 by tsodre-p         ###   ########.fr       */
+/*   Updated: 2023/11/02 16:46:39 by tsodre-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/Point.hpp"
+
+bool bsp( Point const a, Point const b, Point const c, Point const point)
+{
+
+}
--- cpp_module_02/ex03/src/main.cpp
+++ cpp_module_02/ex03/src/main.cpp
@@ -6,7 +6,16 @@
 /*   By: tsodre-p <tsodre-p@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 16:05:20 by tsodre-p          #+#    #+#             */
-/*   Updated: 2023/11/02 16:05:21 by tsodre-p         ###   ########.fr       */
+/*   Updated: 2023/11/02 16:45:48 by tsodre-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/Point.hpp"
+
+int	main (void)
+{
+	Point const a(0,0);
+	Point const b(6,0);
+	Point const c(3,3);
+	Point const check(2,2);
+}
