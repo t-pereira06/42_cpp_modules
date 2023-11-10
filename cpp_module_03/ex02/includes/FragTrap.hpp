a73f402d9929a9c1babbc148dee94b3bdb6e34aa
tsodre-p <tsodre-p@student.42porto.com>
2023-11-10 12:17:14 +0000

    changes to ex02 files

--- cpp_module_03/ex02/includes/FragTrap.hpp
+++ cpp_module_03/ex02/includes/FragTrap.hpp
@@ -6,14 +6,14 @@
 /*   By: tsodre-p <tsodre-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 11:22:55 by tsodre-p          #+#    #+#             */
-/*   Updated: 2023/11/10 11:31:24 by tsodre-p         ###   ########.fr       */
+/*   Updated: 2023/11/10 12:16:35 by tsodre-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRAGTRAP_HPP
 # define FRAGTRAP_HPP
 
-#include "ScavTrap.hpp"
+#include "ClapTrap.hpp"
 
 class FragTrap : public ClapTrap
 {
