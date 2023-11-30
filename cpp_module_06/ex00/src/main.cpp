a16d44b5050f8adf580f97ae6982f30f4974a1d9
tsodre-p <tsodre-p@student.42porto.com>
2023-11-30 10:08:41 +0000

    final changes

--- cpp_module_06/ex00/src/main.cpp
+++ cpp_module_06/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tsodre-p <tsodre-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 10:09:00 by tsodre-p          #+#    #+#             */
-/*   Updated: 2023/11/29 16:57:37 by tsodre-p         ###   ########.fr       */
+/*   Updated: 2023/11/30 09:18:16 by tsodre-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,11 @@
 
 int main(int argc, char **argv)
 {
-	std::string	literal(argv[1]);
-
 	if (argc != 2)
 	{
 		std::cerr << "Error: Run the program with 2 arguments! ./convert <something>" << std::endl;
 		return (0);
 	}
-	ScalarConverter::convert(literal);
+	std::string	arg(argv[1]);
+	ScalarConverter::convert(arg);
 }
