385c431b7648b5c8d2e08032dd237dac9fbde65e
tsodre-p <tsodre-p@student.42porto.com>
2023-12-04 10:24:15 +0000

    using reinterpret cast

--- cpp_module_06/ex01/src/Serializer.cpp
+++ cpp_module_06/ex01/src/Serializer.cpp
@@ -6,7 +6,7 @@
 /*   By: tsodre-p <tsodre-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 09:16:30 by tsodre-p          #+#    #+#             */
-/*   Updated: 2023/11/30 11:56:26 by tsodre-p         ###   ########.fr       */
+/*   Updated: 2023/12/04 10:23:47 by tsodre-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,10 +33,10 @@ Serializer	&Serializer::operator=(Serializer const &copy)
 
 uintptr_t	Serializer::serialize(Data* ptr)
 {
-	return ((uintptr_t)ptr);
+	return (reinterpret_cast<uintptr_t>(ptr));
 }
 
 Data*	Serializer::deserialize(uintptr_t raw)
 {
-	return ((Data*)raw);
+	return (reinterpret_cast<Data*>(raw));
 }
