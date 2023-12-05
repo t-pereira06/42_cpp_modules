76fc8993d8c8cb090251188075b4061f9b445ffb
tsodre-p <tsodre-p@student.42porto.com>
2023-12-05 09:52:43 +0000

    adding ex01 files

---
+++ cpp_module_07/ex01/includes/iter.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsodre-p <tsodre-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 09:25:30 by tsodre-p          #+#    #+#             */
+/*   Updated: 2023/12/05 09:47:03 by tsodre-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+#include <iostream>
+
+template <typename T> void print(T a)
+{
+	std::cout << a << ", ";
+}
+
+template <typename T> void decrement(T a)
+{
+	a--;
+	::print(a);
+}
+
+template <typename T> void iter(T *array, int lenght, void (*func)(T))
+{
+	for (int i = 0; i < lenght; i++)
+		func(array[i]);
+}
+
+
+#endif
---
+++ cpp_module_07/ex01/src/main.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsodre-p <tsodre-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 09:25:19 by tsodre-p          #+#    #+#             */
+/*   Updated: 2023/12/05 09:52:11 by tsodre-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/iter.hpp"
+
+int	main(void)
+{
+	{
+		std::cout << "INT ARRAY TEST" << std::endl;
+		int	array[8] = {99, 4, 5, 2, 43, 53, 67, 72};
+		std::cout << "Before decrementing: " << std::endl;
+		::iter(array, 8, ::print);
+		std::cout << std::endl;
+		std::cout << "After decrementing: " << std::endl;
+		::iter(array, 8, ::decrement);
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+	{
+		std::cout << "STRING ARRAY TEST" << std::endl;
+		std::string	array[4] = {"Hi", "I'm Cracked", "I'm a robot", "and I like to scare people"};
+		::iter(array, 4, ::print);
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+	{
+		std::cout << "DOUBLE ARRAY TEST" << std::endl;
+		double	array[4] = {92.1, 77.4, 44.2, 10.76};
+		std::cout << "Before decrementing: " << std::endl;
+		::iter(array, 4, ::print);
+		std::cout << std::endl;
+		std::cout << "After decrementing: " << std::endl;
+		::iter(array, 4, ::decrement);
+		std::cout << std::endl;
+	}
+}
