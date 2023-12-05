4a913d1e42f78845648f424a8e00af44b107ba9c
tsodre-p <tsodre-p@student.42porto.com>
2023-12-05 15:05:04 +0000

    adding ex02 files

---
+++ cpp_module_07/ex02/includes/Array.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsodre-p <tsodre-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 14:46:00 by tsodre-p          #+#    #+#             */
+/*   Updated: 2023/12/05 15:04:14 by tsodre-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+#include <iostream>
+#include "Array.tpp"
+
+template <class T> class Array
+{
+	private:
+		T	*array;
+		unsigned int	number;
+	public:
+		/* Orthodox Canonical Form */
+		Array();
+		~Array();
+		Array(Array const &copy);
+		Array	&operator=(Array const &copy);
+		/* ----------------------------------------- */
+		Array(unsinged int n);
+		size_t	size() const;
+		T	&operator[](unsigned int index);
+		class	OutOfBounds : public std::exception
+		{
+			public:
+				const char *what() const throw();
+		};
+};
+
+#endif
---
+++ cpp_module_07/ex02/includes/Array.tpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.tpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsodre-p <tsodre-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 14:44:03 by tsodre-p          #+#    #+#             */
+/*   Updated: 2023/12/05 15:03:47 by tsodre-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_TPP
+# define ARRAY_TPP
+
+#include <iostream>
+
+template <class T> Array<T>::Array()
+{
+
+}
+
+template <class T> Array<T>::Array()
+{
+
+}
+
+#endif
