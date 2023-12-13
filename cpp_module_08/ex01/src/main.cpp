c4bb9f3e9caf09c37599052530a984154e411cbc
Tiago Pereira <tiagosodrepereira@gmail.com>
2023-12-13 16:41:48 +0000

    small changes

--- cpp_module_08/ex01/src/main.cpp
+++ cpp_module_08/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tsodre-p <tsodre-p@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 15:29:03 by tsodre-p          #+#    #+#             */
-/*   Updated: 2023/12/13 16:23:35 by tsodre-p         ###   ########.fr       */
+/*   Updated: 2023/12/13 16:40:08 by tsodre-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,10 @@ int	main (void)
 	{
 		Span sp = Span(4);
 
-		sp.addNumber(32);
-		sp.addNumber(71);
+		sp.addNumber(-32);
+		sp.addNumber(-71);
 		sp.addNumber(12);
-		sp.addNumber(45);
+		sp.addNumber(-45);
 		sp.printVector();
 		std::cout << "The longest span is " << sp.longestSpan() << std::endl;
 	}
@@ -42,7 +42,4 @@ int	main (void)
 	{
 		std::cerr << e.what() << '\n';
 	}
-
-
-
 }
