d2c0ed23e9d93e6b480eb339c16c30635bfc7feb
tsodre-p <tsodre-p@student.42porto.com>
2023-12-21 14:53:37 +0000

    changes

--- cpp_module_09/ex02/includes/PmergeMe.hpp
+++ cpp_module_09/ex02/includes/PmergeMe.hpp
@@ -6,23 +6,32 @@
 /*   By: tsodre-p <tsodre-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 14:26:39 by tsodre-p          #+#    #+#             */
-/*   Updated: 2023/12/21 14:26:44 by tsodre-p         ###   ########.fr       */
+/*   Updated: 2023/12/21 14:36:56 by tsodre-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef RPN_HPP
+# define RPN_HPP
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <list>
+#include <stdio.h>
+#include <string.h>
+
 class PmergeMe
 {
-private:
-	/* data */
-public:
-	PmergeMe(/* args */);
-	~PmergeMe();
+	private:
+		std::vector<int> vector;
+		std::list<int> list;
+	public:
+		/* Orthodox Canonical Form */
+		PmergeMe();
+		~PmergeMe();
+		PmergeMe(PmergeMe const &copy);
+		PmergeMe	&operator=(PmergeMe const &copy);
+		/* ----------------------- */
 };
 
-PmergeMe::PmergeMe(/* args */)
-{
-}
-
-PmergeMe::~PmergeMe()
-{
-}
+#endif
